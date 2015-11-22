62d4852e84b847d515f96cecf722f83051677301
SaintPeter <rex.schrader@gmail.com>
2015-11-22 00:15:02 -0800

    Windows Compatibility Changes

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loaders: ['babel'],
+        loaders: ['babel-loader'],
         include: path.join(__dirname, 'src')
       },
       {
