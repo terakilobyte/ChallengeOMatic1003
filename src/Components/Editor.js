f6034400f14feb45005a86e1150464c908c8ce03
SaintPeter <rex.schrader@gmail.com>
2015-11-23 21:27:19 -0800

    Fix Description Munging, Test Munging, and Global Save State Issues

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,5 +1,5 @@
 import './../../node_modules/codemirror/lib/codemirror.css';
-import './../../node_modules/codemirror/theme/dracula.css';
+import './../../node_modules/codemirror/theme/monokai.css';
 import './../../node_modules/codemirror/addon/scroll/simplescrollbars.css';
 
 import React, {Component} from 'react';
@@ -10,23 +10,32 @@ import {connect} from 'react-redux';
 
 import CodeMirror from './../../node_modules/codemirror/lib/codemirror';
 import './../../node_modules/codemirror/mode/javascript/javascript';
+import './../../node_modules/codemirror/mode/xml/xml';
+import './../../node_modules/codemirror/mode/css/css';
+import './../../node_modules/codemirror/mode/htmlmixed/htmlmixed';
 import './../../node_modules/codemirror/addon/edit/closebrackets';
 import './../../node_modules/codemirror/addon/edit/matchbrackets';
 import './../../node_modules/codemirror/addon/scroll/simplescrollbars';
 import './../../node_modules/codemirror/addon/scroll/annotatescrollbar';
 import './../../node_modules/codemirror/addon/scroll/scrollpastend';
 import './../../node_modules/codemirror/addon/lint/lint';
+import './../../node_modules/codemirror/addon/lint/javascript-lint';
 
 const connector = connect(function(state, props){
   //State from redux
-  return ({challenge: state.challenges.reduce(function(prevC, challenge){
-    if(challenge.id === props.id) {
-      return(challenge)
+  return (
+    {
+      challenge: state.challenges.reduce(function(prevC, challenge){
+        if(challenge.id === props.id) {
+          return(challenge)
+        }
+        else {
+          return(prevC);
+        }
+      }, {}),
+      activeFile: state.activeFile
     }
-    else {
-      return(prevC);
-    }
-  }, {})});
+  );
 });
 
 class Editor extends Component {
@@ -48,8 +57,12 @@ class Editor extends Component {
     });
 
     unrenderedCodeMirrors = codeMirrorData.map(function(data){
-      if(Array.isArray(data[1])){
-        data[1] = data[1].join('\n')
+      if (Array.isArray(data[1])){
+        if (data[0] === 'tests') {
+          data[1] = data[1].join('EOL\n');
+        } else {
+          data[1] = data[1].join('\n');
+        }
      }
      return(
      <div key = {data[0]}>
@@ -69,12 +82,24 @@ class Editor extends Component {
     let codeMirrors = [];
     const dispatch = this.props.dispatch;
     const challengeId = this.props.challenge.id;
+    const activeFile = this.props.activeFile;
+    const challengeType = this.props.challenge.challengeType;
 
-    this.state.codeMirrorData.map(function(codeMirror){
+    this.state.codeMirrorData.map(function(codeMirror) {
+      // Determine mode
+      let mode = 'text';
+      if (codeMirror[0] === 'tests' ||
+          ((codeMirror[0] === 'challengeSeed' ||
+          codeMirror[0] === 'solutions') && challengeType === 5)) {
+            mode = 'javascript';
+          } else {
+            mode = 'htmlmixed';
+          }
+
       let editor = CodeMirror.fromTextArea(document.getElementById(codeMirror[0]), {
         lineNumbers: true,
-        mode: 'javascript',
-        theme: 'dracula',
+        mode: mode || 'text',
+        theme: 'monokai',
         runnable: true,
         matchBrackets: true,
         autoCloseBrackets: true,
@@ -84,13 +109,13 @@ class Editor extends Component {
       });
 
       editor.on('change', function(instance, changeObj){
-
         updateChallenge(dispatch,
           {
             id: challengeId,
             props: {
-              [codeMirror[0]]: instance.getValue() + changeObj.text
-            }
+              [codeMirror[0]]: instance.getValue()
+            },
+            activeFile: activeFile
 
           }
         );
