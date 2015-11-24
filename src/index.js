a1bb213174692d32ee90cc25adfad805a8a98f03
terakilobyte <terakilobyte@gmail.com>
2015-11-24 05:14:58 -0800

    lint index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import App from './App';
 import {Provider} from 'react-redux';
 import store from './store';
 
-ReactDOM.render(<Provider store = {store} ><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store = {store} ><App /></Provider>,
+  document.getElementById('root')
+
+);
